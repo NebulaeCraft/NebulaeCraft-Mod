07dc5adc42c315de64bdf0376926da7acc176e5d
kirakiseki <kirakiseki@163.com>
2022-06-16 01:25:07 +0800

    test: change CameraCart hitbox

--- src/main/java/net/kuina/nebulaecraft/entities/EntityCameraCart.java
+++ src/main/java/net/kuina/nebulaecraft/entities/EntityCameraCart.java
@@ -1,24 +1,35 @@
 package net.kuina.nebulaecraft.entities;
 
+import net.kuina.nebulaecraft.util.MinecartCollisionHandler;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityMinecartEmpty;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.world.World;
 
+import javax.annotation.Nullable;
+
 public class EntityCameraCart extends EntityMinecartEmpty {
+
+    private static final net.minecraftforge.common.IMinecartCollisionHandler collisionHandler = new MinecartCollisionHandler();
+
     public EntityCameraCart(World worldIn)
     {
         super(worldIn);
+        setCollisionHandler(collisionHandler);
     }
 
     public EntityCameraCart(World worldIn, double x, double y, double z)
     {
         super(worldIn, x, y, z);
+        setCollisionHandler(collisionHandler);
     }
 
     @Override
     public double getMountedYOffset()
     {
-        return 0.9D;
+        return 0.875D;
     }
+
 
 }
 
