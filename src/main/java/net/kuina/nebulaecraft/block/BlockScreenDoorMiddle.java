eafbfe3872427e93a9960712a1b26b22d0567dcc
Kuina20 <kuina@vip.qq.com>
2023-09-02 23:17:59 +0800

    fix: No BoundingBox for Screen Wall Middle part

--- src/main/java/net/kuina/nebulaecraft/block/BlockScreenDoorMiddle.java
+++ src/main/java/net/kuina/nebulaecraft/block/BlockScreenDoorMiddle.java
@@ -68,12 +68,6 @@ public class BlockScreenDoorMiddle extends ElementsNebulaecraftMod.ModElement {
         }
 
         @Override
-        @javax.annotation.Nullable
-        public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
-            return NULL_AABB;
-        }
-
-        @Override
         @SideOnly(Side.CLIENT)
         public void getSubBlocks(CreativeTabs whichTab, NonNullList<ItemStack> items) {
             BlockScreenDoorMiddle.BlockCustom.EnumType[] allSubtypes = BlockScreenDoorMiddle.BlockCustom.EnumType.values();
