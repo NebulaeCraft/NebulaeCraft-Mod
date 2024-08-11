6e776c9bcf02042a4bffce3c6eddc6b42f33696c
Kuina20 <kuina@vip.qq.com>
2024-08-11 14:12:20 +0800

    修改了一些MC原版的GUI

--- src/main/java/net/kuina/nebulaecraft/util/RegistryHandler.java
+++ src/main/java/net/kuina/nebulaecraft/util/RegistryHandler.java
@@ -5,7 +5,7 @@ import net.kuina.nebulaecraft.block.*;
 import net.kuina.nebulaecraft.creativetab.TabNebulaecraftMetro;
 import net.kuina.nebulaecraft.entities.*;
 import net.kuina.nebulaecraft.tileentity.TileEntityTdt;
-import net.kuina.nebulaecraft.util.ServerHandler;
+//import net.kuina.nebulaecraft.util.ServerHandler;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
@@ -52,11 +52,11 @@ public class RegistryHandler extends ElementsNebulaecraftMod.ModElement {
         GameRegistry.registerTileEntity(tileEntityClass, tileEntityName);
     }
 
-    ServerHandler handler = new ServerHandler();
+//    ServerHandler handler = new ServerHandler();
 
     @Override
     public void initElements() {
-        System.out.println(ServerHandler.CornField);
+//        System.out.println(ServerHandler.CornField);
         for (int i = 1; i <= 4; i++) {
             registerBlock(new BlockScreenDoorBig.BlockCustom(), "screen_door_" + i);
         }
@@ -144,6 +144,8 @@ public class RegistryHandler extends ElementsNebulaecraftMod.ModElement {
         registerBlock(new BlockRoadmarkArrow.BlockCustom(), "roadmark_arrow_back");
         registerBlock(new BlockRoadmarkArrow.BlockCustom(), "roadmark_arrow_halfright");
     	registerBlock(new BlockRoadmarkArrow.BlockCustom(), "roadmark_arrow_halfleft");
+    	registerBlock(new BlockRoadmarkArrow.BlockCustom(), "roadmark_arrow_left_back");
+    	registerBlock(new BlockRoadmarkArrow.BlockCustom(), "roadmark_arrow_noback");
 
         registerItem(new CameraCart.ItemCameraCart(), "cameracart");
 
