b1cfe329d01f7b5154a4281cd08a85809ba6659a
Kuina20 <kuina@vip.qq.com>
2026-06-12 20:58:28 +0800

    feat: 防眩板半砖版本

--- src/main/java/net/kuina/nebulaecraft/block/BlockAntiglareShield.java
+++ src/main/java/net/kuina/nebulaecraft/block/BlockAntiglareShield.java
@@ -1,4 +1,3 @@
-
 package net.kuina.nebulaecraft.block;
 
 import net.kuina.nebulaecraft.ElementsNebulaecraftMod;
@@ -8,17 +7,18 @@ import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
-import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.Mirror;
-import net.minecraft.util.Rotation;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.*;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.ModelRegistryEvent;
@@ -29,93 +29,171 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 @ElementsNebulaecraftMod.ModElement.Tag
 public class BlockAntiglareShield extends ElementsNebulaecraftMod.ModElement {
-	@GameRegistry.ObjectHolder("nebulaecraft:antiglare_shield")
-	public static final Block block = null;
-	public BlockAntiglareShield(ElementsNebulaecraftMod instance) {
-		super(instance, 24);
-	}
+    @GameRegistry.ObjectHolder("nebulaecraft:antiglare_shield")
+    public static final Block block = null;
 
-	@Override
-	public void initElements() {
-		elements.blocks.add(() -> new BlockCustom().setRegistryName("antiglare_shield"));
-		elements.items.add(() -> new ItemBlock(block).setRegistryName(block.getRegistryName()));
-	}
+    public BlockAntiglareShield(ElementsNebulaecraftMod instance) {
+        super(instance, 24);
+    }
 
-	@SideOnly(Side.CLIENT)
-	@Override
-	public void registerModels(ModelRegistryEvent event) {
-		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0,
-				new ModelResourceLocation("nebulaecraft:antiglare_shield", "inventory"));
-	}
-	public static class BlockCustom extends Block {
-		public static final PropertyDirection FACING = BlockHorizontal.FACING;
-		public BlockCustom() {
-			super(Material.IRON);
-			setUnlocalizedName("antiglare_shield");
-			setSoundType(SoundType.METAL);
-			setHardness(1F);
-			setResistance(10F);
-			setLightLevel(0F);
-			setLightOpacity(0);
-			setCreativeTab(TabNebulaecraftRoad.tab);
-			this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
-		}
+    @Override
+    public void initElements() {
+        elements.blocks.add(() -> new BlockCustom().setRegistryName("antiglare_shield"));
+        elements.items.add(() -> new ItemHasVariantsAndSubtypes(block).setSubtypeNames(new String[]{"subtype0", "subtype1"}).setRegistryName(block.getRegistryName()));
+    }
 
-		@SideOnly(Side.CLIENT)
-		@Override
-		public BlockRenderLayer getBlockLayer() {
-			return BlockRenderLayer.CUTOUT_MIPPED;
-		}
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerModels(ModelRegistryEvent event) {
+        BlockAntiglareShield.BlockCustom.EnumType[] allSubtypes = BlockAntiglareShield.BlockCustom.EnumType.values();
+        for (BlockAntiglareShield.BlockCustom.EnumType subtype : allSubtypes) {
+            ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), subtype.getMetadata(), new ModelResourceLocation("nebulaecraft:antiglare_shield_" + subtype.getName(), "inventory"));
+        }
+    }
 
-		@Override
-		public boolean isFullCube(IBlockState state) {
-			return false;
-		}
+    public static class BlockCustom extends Block {
+        public static final PropertyDirection FACING = BlockHorizontal.FACING;
+        public static final PropertyEnum<EnumType> SUBTYPE = PropertyEnum.create("subtype", EnumType.class);
 
-		@Override
-		public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-			if (state.getValue(BlockHorizontal.FACING) == EnumFacing.NORTH || state.getValue(BlockHorizontal.FACING) == EnumFacing.SOUTH) {
-				return new AxisAlignedBB(0.375, 0, 0, 0.625, 0.625, 1);
-			}
-			else {
-				return new AxisAlignedBB(0, 0, 0.375, 1, 0.625, 0.625);
+        public BlockCustom() {
+            super(Material.IRON);
+            setUnlocalizedName("antiglare_shield");
+            setSoundType(SoundType.METAL);
+            setHardness(1F);
+            setResistance(10F);
+            setLightLevel(0F);
+            setLightOpacity(0);
+            setCreativeTab(TabNebulaecraftRoad.tab);
+            this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+        }
+
+        @Override
+        @SideOnly(Side.CLIENT)
+        public void getSubBlocks(CreativeTabs whichTab, NonNullList<ItemStack> items) {
+            BlockAntiglareShield.BlockCustom.EnumType[] allSubtypes = BlockAntiglareShield.BlockCustom.EnumType.values();
+            for (BlockAntiglareShield.BlockCustom.EnumType subtype : allSubtypes) {
+                items.add(new ItemStack(this, 1, subtype.getMetadata()));
             }
-		}
+        }
 
-		@Override
-		protected net.minecraft.block.state.BlockStateContainer createBlockState() {
-			return new net.minecraft.block.state.BlockStateContainer(this, FACING);
-		}
+        @Override
+        public BlockRenderLayer getBlockLayer() {
+            return BlockRenderLayer.CUTOUT_MIPPED;
+        }
 
-		@Override
-		public IBlockState withRotation(IBlockState state, Rotation rot) {
-			return state.withProperty(FACING, rot.rotate(state.getValue(FACING)));
-		}
+        @Override
+        public boolean isFullCube(IBlockState state) {
+            return false;
+        }
 
-		@Override
-		public IBlockState withMirror(IBlockState state, Mirror mirrorIn) {
-			return state.withRotation(mirrorIn.toRotation(state.getValue(FACING)));
-		}
+        @Override
+        public boolean isOpaqueCube(IBlockState state) {
+            return false;
+        }
 
-		@Override
-		public IBlockState getStateFromMeta(int meta) {
-			return this.getDefaultState().withProperty(FACING, EnumFacing.getFront(meta+2));
-		}
+        @Override
+        public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+            if(state.getValue(SUBTYPE).getMetadata()==0){
+                if (state.getValue(BlockHorizontal.FACING) == EnumFacing.NORTH || state.getValue(BlockHorizontal.FACING) == EnumFacing.SOUTH) {
+                    return new AxisAlignedBB(0.375, 0, 0, 0.625, 0.625, 1);
+                }
+                else {
+                    return new AxisAlignedBB(0, 0, 0.375, 1, 0.625, 0.625);
+                }
+            }
+            else{
+                if (state.getValue(BlockHorizontal.FACING) == EnumFacing.NORTH || state.getValue(BlockHorizontal.FACING) == EnumFacing.SOUTH) {
+                    return new AxisAlignedBB(0.375, -0.5, 0, 0.625, 0.125, 1);
+                }
+                else {
+                    return new AxisAlignedBB(0, -0.5, 0.375, 1, 0.125, 0.625);
+                }
+            }
+        }
 
-		@Override
-		public int getMetaFromState(IBlockState state) {
-			return (state.getValue(FACING).getIndex()-2);
-		}
+        @Override
+        protected net.minecraft.block.state.BlockStateContainer createBlockState() {
+            return new net.minecraft.block.state.BlockStateContainer(this, FACING, SUBTYPE);
+        }
 
-		@Override
-		public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta,
-				EntityLivingBase placer) {
-			return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
-		}
+        @Override
+        public IBlockState withRotation(IBlockState state, Rotation rot) {
+            return state.withProperty(FACING, rot.rotate(state.getValue(FACING)));
+        }
 
-		@Override
-		public boolean isOpaqueCube(IBlockState state) {
-			return false;
-		}
-	}
+        @Override
+        public IBlockState withMirror(IBlockState state, Mirror mirrorIn) {
+            return state.withRotation(mirrorIn.toRotation(state.getValue(FACING)));
+        }
+
+        @Override
+        public IBlockState getStateFromMeta(int meta) {
+            return this.getDefaultState().withProperty(FACING, EnumFacing.getFront((meta % 4) + 2)).withProperty(SUBTYPE, EnumType.byMetadata(meta / 4));
+        }
+
+        @Override
+        public int getMetaFromState(IBlockState state) {
+            int metadata=((state.getValue(FACING).getIndex() - 2) + (4 * state.getValue(SUBTYPE).getMetadata()));
+            return metadata;
+        }
+
+        @Override
+        public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player) {
+            super.getPickBlock(state, target, world, pos, player);
+            return new ItemStack(this,1,getMetaFromState(state)/4);
+        }
+
+        @Override
+        public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+            EnumType subtype = EnumType.byMetadata(meta);
+
+            if (facing == EnumFacing.UP || facing == EnumFacing.DOWN)
+                return this.getDefaultState().withProperty(SUBTYPE, subtype).withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+
+//            System.out.println(EnumFacing.getFront(facing.getIndex() - 2).getName2());
+            return this.getDefaultState().withProperty(FACING, facing).withProperty(SUBTYPE, subtype);
+        }
+
+        public enum EnumType implements IStringSerializable {
+            SUBTYPE0(0, "subtype0"),
+            SUBTYPE1(1, "subtype1");
+
+            private static final BlockAntiglareShield.BlockCustom.EnumType[] META_LOOKUP = new BlockAntiglareShield.BlockCustom.EnumType[values().length];
+
+            static {
+                for (BlockAntiglareShield.BlockCustom.EnumType type : values()) {
+                    META_LOOKUP[type.getMetadata()] = type;
+                }
+            }
+
+            private final int meta;
+            private final String name;
+
+            EnumType(int i_meta, String i_name) {
+                this.meta = i_meta;
+                this.name = i_name;
+            }
+
+            public static BlockAntiglareShield.BlockCustom.EnumType byMetadata(int meta) {
+                if (meta < 0 || meta >= META_LOOKUP.length) {
+                    meta = 0;
+                }
+
+                return META_LOOKUP[meta];
+            }
+
+            public int getMetadata() {
+                return this.meta;
+            }
+
+            @Override
+            public String toString() {
+                return this.name;
+            }
+
+            public String getName() {
+                return this.name;
+            }
+        }
+    }
 }
