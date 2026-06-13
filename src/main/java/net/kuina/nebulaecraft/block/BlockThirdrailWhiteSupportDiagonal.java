5ac1c7d078063265e1322cd4b8d7a9063426863a
Kuina20 <kuina@vip.qq.com>
2026-06-13 10:36:04 +0800

    feat: 第三轨斜线支架

---
+++ src/main/java/net/kuina/nebulaecraft/block/BlockThirdrailWhiteSupportDiagonal.java
@@ -0,0 +1,126 @@
+
+package net.kuina.nebulaecraft.block;
+
+import net.kuina.nebulaecraft.ElementsNebulaecraftMod;
+import net.kuina.nebulaecraft.creativetab.TabNebulaecraftMetro;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockHorizontal;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@ElementsNebulaecraftMod.ModElement.Tag
+public class BlockThirdrailWhiteSupportDiagonal extends ElementsNebulaecraftMod.ModElement {
+	@GameRegistry.ObjectHolder("nebulaecraft:thirdrail_white_support_diagonal")
+	public static final Block block = null;
+	public BlockThirdrailWhiteSupportDiagonal(ElementsNebulaecraftMod instance) {
+		super(instance, 29);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new BlockCustom().setRegistryName("thirdrail_white_support_diagonal"));
+		elements.items.add(() -> new ItemBlock(block).setRegistryName(block.getRegistryName()));
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0,
+				new ModelResourceLocation("nebulaecraft:thirdrail_white_support_diagonal", "inventory"));
+	}
+	public static class BlockCustom extends Block {
+		public static final PropertyDirection FACING = BlockHorizontal.FACING;
+		public BlockCustom() {
+			super(Material.IRON);
+			setUnlocalizedName("thirdrail_white_support_diagonal");
+			setSoundType(SoundType.METAL);
+			setHardness(1F);
+			setResistance(10F);
+			setLightLevel(0F);
+			setLightOpacity(0);
+			setCreativeTab(TabNebulaecraftMetro.tab);
+			this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+		}
+
+		@SideOnly(Side.CLIENT)
+		@Override
+		public BlockRenderLayer getBlockLayer() {
+			return BlockRenderLayer.CUTOUT_MIPPED;
+		}
+
+		@Override
+		public boolean isFullCube(IBlockState state) {
+			return false;
+		}
+
+		@Override
+		public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+			switch (state.getValue(BlockHorizontal.FACING)) {
+				case SOUTH :
+					default :
+						return new AxisAlignedBB(0, 0.25, 0, 0.875, 0.4375, 0.875);
+					case NORTH :
+						return new AxisAlignedBB(0.125, 0.25, 0.125, 1, 0.4375, 1);
+					case EAST :
+						return new AxisAlignedBB(0, 0.25, 1, 0.875, 0.4375, 0.125);
+					case WEST :
+						return new AxisAlignedBB(1, 0.25, 0, 0.125, 0.4375, 0.875);
+			}
+		}
+
+		@Override
+		protected net.minecraft.block.state.BlockStateContainer createBlockState() {
+			return new net.minecraft.block.state.BlockStateContainer(this, FACING);
+		}
+
+		@Override
+		public IBlockState withRotation(IBlockState state, Rotation rot) {
+			return state.withProperty(FACING, rot.rotate(state.getValue(FACING)));
+		}
+
+		@Override
+		public IBlockState withMirror(IBlockState state, Mirror mirrorIn) {
+			return state.withRotation(mirrorIn.toRotation(state.getValue(FACING)));
+		}
+
+		@Override
+		public IBlockState getStateFromMeta(int meta) {
+			return this.getDefaultState().withProperty(FACING, EnumFacing.getFront(meta+2));
+		}
+
+		@Override
+		public int getMetaFromState(IBlockState state) {
+			return (state.getValue(FACING).getIndex()-2);
+		}
+
+		@Override
+		public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta,
+				EntityLivingBase placer) {
+			return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+		}
+
+		@Override
+		public boolean isOpaqueCube(IBlockState state) {
+			return false;
+		}
+	}
+}
