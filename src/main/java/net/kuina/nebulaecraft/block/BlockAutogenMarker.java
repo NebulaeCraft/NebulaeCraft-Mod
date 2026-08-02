38624880faa43d52cc7e5cfe82311b62cf74f86c
Kuina20 <kuina@vip.qq.com>
2026-08-02 12:45:03 +0800

    feat: 新的自动生成器模型和贴图

--- src/main/java/net/kuina/nebulaecraft/block/BlockAutogenMarker.java
+++ src/main/java/net/kuina/nebulaecraft/block/BlockAutogenMarker.java
@@ -13,11 +13,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.Mirror;
-import net.minecraft.util.Rotation;
+import net.minecraft.util.*;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentTranslation;
@@ -98,6 +94,11 @@ public class BlockAutogenMarker extends Block {
     @Override
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
         return state.getValue(FACING).getAxis() == EnumFacing.Axis.Z ? BOX_NS : BOX_EW;
+    }
+
+    @Override
+    public BlockRenderLayer getBlockLayer() {
+        return BlockRenderLayer.CUTOUT_MIPPED;
     }
 
     @Override
